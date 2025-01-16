4be60542001772774e4ba86a45ec5b266f75ce6e
buymoney10 <149011343+buymoney10@users.noreply.github.com>
2025-01-16 09:06:25 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contentSection = document.getElementById('content');
+    contentSection.innerHTML += `
+        <p>JavaScript is working! You can add more interactive features here.</p>
+    `;
+});
